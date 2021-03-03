d486c0633303d8b5b99eefc0199b185b2bdc444e
Cesar Benavides <cesarbenavides@icloud.com>
2021-03-03 08:54:45 -0600

    gatsby-image tweak fix

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image";
+// import { GatsbyImage } from "gatsby-plugin-image";
 
 // import styles from './article-preview.module.css'
 
 export default ({ article }) => (
   <div>
-    <GatsbyImage image={article.childImageSharp.gatsbyImageData} alt="" />
+    {/* <GatsbyImage image={article.childImageSharp.gatsbyImageData} alt="" /> */}
     <h3>
       <Link to={`/blog/${article.slug}`}>{article.title}</Link>
     </h3>
